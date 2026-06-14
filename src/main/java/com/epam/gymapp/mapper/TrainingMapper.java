936e979fb2872b4049bdae71b38a9824073b79f5
Arman Melkonyan <melkonyan.arman.03@gmail.com>
2026-06-14 22:18:12 +0400

    Fix mapper

--- src/main/java/com/epam/gymapp/mapper/TrainingMapper.java
+++ src/main/java/com/epam/gymapp/mapper/TrainingMapper.java
@@ -4,6 +4,7 @@ import com.epam.gymapp.dto.training.TrainingCreateDto;
 import com.epam.gymapp.dto.training.TrainingDto;
 import com.epam.gymapp.persistence.entity.Training;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -14,6 +15,8 @@ public interface TrainingMapper {
 
     Training mapCreateToTraining(TrainingCreateDto dto);
 
+    @Mapping(target = "trainerUsername", source = "training.trainer.user.username")
+    @Mapping(target = "traineeUsername", source = "training.trainee.user.username")
     TrainingDto mapToDto(Training training);
 
     List<TrainingDto> mapToDtoList(List<Training> trainingsByCriteria);
