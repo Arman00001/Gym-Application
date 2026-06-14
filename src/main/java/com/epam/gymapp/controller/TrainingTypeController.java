53b6c3241b420b404ab811f75461e5e4c33a1ab8
Arman Melkonyan <melkonyan.arman.03@gmail.com>
2026-06-14 22:18:29 +0400

    Add proper naming to the controller's documentation

--- src/main/java/com/epam/gymapp/controller/TrainingTypeController.java
+++ src/main/java/com/epam/gymapp/controller/TrainingTypeController.java
@@ -2,6 +2,7 @@ package com.epam.gymapp.controller;
 
 import com.epam.gymapp.dto.trainingtype.TrainingTypeDto;
 import com.epam.gymapp.service.trainingtype.TrainingTypeService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "/training-types", produces = {"application/JSON"})
+@Tag(name = "Training Types", description = "Operations for retrieving training types in the application")
 @RequiredArgsConstructor
 public class TrainingTypeController {
 
     private final TrainingTypeService trainingTypeService;
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<TrainingTypeDto>> getAll() {
         return ResponseEntity.ok(trainingTypeService.getAll());
     }
