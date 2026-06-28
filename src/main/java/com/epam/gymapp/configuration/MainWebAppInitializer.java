6aedea3c0079eb888c6bef70b79e530a1a969edb
Arman Melkonyan <melkonyan.arman.03@gmail.com>
2026-06-28 09:24:40 +0400

    Code cleanup

--- src/main/java/com/epam/gymapp/configuration/MainWebAppInitializer.java
+++ src/main/java/com/epam/gymapp/configuration/MainWebAppInitializer.java
@@ -1,47 +1,47 @@
-package com.epam.gymapp.configuration;
-
-import jakarta.servlet.FilterRegistration;
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRegistration;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.DispatcherServlet;
-
-public class MainWebAppInitializer implements WebApplicationInitializer {
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-
-        AnnotationConfigWebApplicationContext rootContext =
-                new AnnotationConfigWebApplicationContext();
-        rootContext.register(AppConfig.class);
-
-        servletContext.addListener(new ContextLoaderListener(rootContext));
-
-        AnnotationConfigWebApplicationContext servletContextConfig =
-                new AnnotationConfigWebApplicationContext();
-        servletContextConfig.register(WebConfig.class);
-
-        ServletRegistration.Dynamic dispatcher =
-                servletContext.addServlet(
-                        "dispatcher",
-                        new DispatcherServlet(servletContextConfig)
-                );
-
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
-
-        FilterRegistration.Dynamic encodingFilter =
-                servletContext.addFilter(
-                        "encoding-filter",
-                        new CharacterEncodingFilter()
-                );
-
-        encodingFilter.setInitParameter("encoding", "UTF-8");
-        encodingFilter.setInitParameter("forceEncoding", "true");
-        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
-    }
-}
+//package com.epam.gymapp.configuration;
+//
+//import jakarta.servlet.FilterRegistration;
+//import jakarta.servlet.ServletContext;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.ServletRegistration;
+//import org.springframework.web.WebApplicationInitializer;
+//import org.springframework.web.context.ContextLoaderListener;
+//import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+//import org.springframework.web.filter.CharacterEncodingFilter;
+//import org.springframework.web.servlet.DispatcherServlet;
+//
+//public class MainWebAppInitializer implements WebApplicationInitializer {
+//
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//
+//        AnnotationConfigWebApplicationContext rootContext =
+//                new AnnotationConfigWebApplicationContext();
+//        rootContext.register(AppConfig.class);
+//
+//        servletContext.addListener(new ContextLoaderListener(rootContext));
+//
+//        AnnotationConfigWebApplicationContext servletContextConfig =
+//                new AnnotationConfigWebApplicationContext();
+//        servletContextConfig.register(WebConfig.class);
+//
+//        ServletRegistration.Dynamic dispatcher =
+//                servletContext.addServlet(
+//                        "dispatcher",
+//                        new DispatcherServlet(servletContextConfig)
+//                );
+//
+//        dispatcher.setLoadOnStartup(1);
+//        dispatcher.addMapping("/");
+//
+//        FilterRegistration.Dynamic encodingFilter =
+//                servletContext.addFilter(
+//                        "encoding-filter",
+//                        new CharacterEncodingFilter()
+//                );
+//
+//        encodingFilter.setInitParameter("encoding", "UTF-8");
+//        encodingFilter.setInitParameter("forceEncoding", "true");
+//        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+//    }
+//}
